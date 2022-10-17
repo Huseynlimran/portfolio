5a795c1290ec6372fd50118d5209c4ae11dad613
Huseynlimran <huseynlimran@gmail.com>
2022-10-17 12:41:27 +0400

    change

--- index.js
+++ index.js
@@ -14,24 +14,45 @@
 //     });
 //   });
 
-const eye1 = document.querySelector(".eye1");
-const eye2 = document.querySelector(".eye2");
-window.addEventListener("mousemove", (evt) => {
-  const x = -(window.innerWidth / 1.5 - evt.pageX) / 10;
-  const y = -(window.innerHeight / 1.5 - evt.pageY) / 10;
-  eye1.style.transform = `translateY(${y}px) translateX(${x}px)`;
-  eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
-});
 
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelectorAll(".nav-links li");
+const linkHref = document.querySelectorAll(".linksNav")
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
-  burger.classList.toggle("toggle");
+if(burger){
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+}
 
-});
+function linkAction(){
+  nav.classList.remove("nav-active")
+  burger.classList.remove("toggle")
+}
+linkHref.forEach(n => n.addEventListener('click', linkAction))
+
+window.addEventListener("scroll", ()=>{
+  let navbar = document.querySelector("nav")
+
+  if(scrollY > 0){
+    navbar.classList.add("sticky")
+  }else{
+    navbar.classList.remove("sticky")
+  }
+
+})
+
+
+document.querySelector(".scroll").addEventListener("click", (e)=>{
+  e.preventDefault();
+  document.body.scrollIntoView({
+    block:'start',
+    inline:'start',
+    behavior:'smooth'
+  })
+})
 
 // const cursor = document.querySelector(".cursor");
 // document.addEventListener("click", () => {
@@ -53,13 +74,60 @@ burger.addEventListener("click", () => {
 
 // intro
 
-const sg = gsap.timeline({default: {ease: 'power1.out'}})
+document.body.addEventListener("mousemove", event =>{
+  const mouseX = event.clientX
+  const mouseY = event.clientY
 
-sg.to(".text", {rotateX:"0deg",duration:.5,opacity:.7, stagger:.1})
+  gsap.to(".cursor",{
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1
+  })
+})
+
+document.body.addEventListener("click", event =>{
+  
+  let cursor = document.querySelector(".cursor")
+
+  cursor.classList.add("expand")
+
+  setTimeout(() => {
+    cursor.classList.remove("expand")
+  }, 300);
+})
+
+
+
+const eye1 = document.querySelector(".eye1");
+const eye2 = document.querySelector(".eye2");
+window.addEventListener("mousemove", (evt) => {
+  const x = -(window.innerWidth / 1.5 - evt.pageX) / 10;
+  const y = -(window.innerHeight / 1.5 - evt.pageY) / 10;
+  eye1.style.transform = `translateY(${y}px) translateX(${x}px)`;
+  eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
+});
+
+// const myList = document.querySelectorAll(".listLi")
+
+// console.log(myList)
+
+// myList.forEach(list=>{
+//   list.addEventListener("click", (e)=>{
+//     [ ... myList].map(list => list.classList.remove("activeLinks"))
+//     list.classList.add("activeLinks")
+//   })
+// })
+
+
+
+const sg = gsap.timeline({ default: { ease: "power1.out" } });
+
+sg.to(".text", { rotateX: "0deg", duration: 0.2, opacity: 0.7, stagger: 0.1 });
 
 sg.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 sg.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 sg.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+sg.fromTo(".sag", { opacity: 0,x:"50%" }, {opacity: 1, duration: 1,x:"0",stagger:.5 },"-=1");
 sg.fromTo(
   ".linksNav",
   { y: "-50%", opacity: 0 },
@@ -67,17 +135,21 @@ sg.fromTo(
   "-=1"
 );
 sg.fromTo(
+  ".sol",{scaleY: 1.5,opacity:0,ease: "elastic.out(2, 0.4)"},{opacity:1, scaleY: 1, ease: "elastic.out(3, 0.4",stagger:.1 },"-=2");
+sg.fromTo(
   ".mensi1",
   { "border-top": "200px solid black", opacity: 0, y: "-15%" },
   { "border-top": "40px solid black", opacity: 1, duration: 1, y: "0%" },
-  "-=1"
+  "-=3"
 );
 sg.fromTo(
   ".mensi2",
   { "border-top": "200px solid black", opacity: 0, y: "-15%" },
   { "border-top": "40px solid black", opacity: 1, duration: 1, y: "0%" },
-  "-=1"
+  "-=3"
 );
+
+
 
 // var rule = CSSRulePlugin.getRule(".eye:hover::after"); //get the rule
 
@@ -98,12 +170,22 @@ boxe.forEach((element) => {
       },
       { duration: 2, scaleY: 1, ease: "elastic.out(3, 0.4" }
     );
-    setTimeout(() => {
-      element.classList.remove("active2");
-    }, 300);
+    element.addEventListener("mouseup", ()=>{
+      element.classList.add("active2")
+    })
+    element.addEventListener("mouseleave", ()=>{
+      element.classList.remove("active2")
+    })
   });
 });
 
+ScrollReveal().reveal(".lefts", {
+  origin: "left",
+  duration: 2000,
+  distance: "5rem",
+  interval: 100,
+  opacity: 0,
+});
 ScrollReveal().reveal(".top", {
   origin: "top",
   duration: 2000,
@@ -118,27 +200,21 @@ ScrollReveal().reveal(".bottom", {
   opacity: 0,
 });
 ScrollReveal().reveal(".left", {
-  origin: "left",
-  duration: 2000,
+  origin: "top",
+  duration: 1500,
   distance: "5rem",
   interval: 100,
   opacity: 0,
+  reset:true
 });
 
-ScrollReveal().reveal(".sag", {
-  origin: "right",
-  duration: 2000,
-  distance: "5rem",
-  interval: 300,
-  opacity: 0,
-});
-ScrollReveal().reveal(".sol", {
-  origin: "left",
-  duration: 100,
-  distance: "1rem",
-  interval: 100,
-  opacity: 0,
-});
+// ScrollReveal().reveal(".sag", {
+//   origin: "right",
+//   duration: 2000,
+//   distance: "5rem",
+//   interval: 300,
+//   opacity: 0,
+// });
 
 // let path= document.querySelector("path")
 // let pathLength = path.getTotalLength()
@@ -156,3 +232,35 @@ ScrollReveal().reveal(".sol", {
 //   path.style.strokeDashoffset = pathLength-drawLength;
 
 // })
+
+
+
+
+const btnContact = document.querySelector(".btnSend")
+const succes = document.querySelector(".success")
+console.log(succes)
+
+btnContact.addEventListener("click", (e)=>{
+  e.preventDefault();
+  succes.classList.add("activeSucces")
+  setTimeout(() => {
+    succes.classList.remove("activeSucces")
+  }, 1500);
+  
+})
+
+
+
+
+
+function sendMail(params) {
+  var tempParams = {
+    from_name: document.getElementById("toEmail").value,
+    to_name : document.getElementById("toName").value,
+    message : document.getElementById("msg").value,
+  };
+  emailjs.send('service_f53gpjf','template_hrkb4b9',tempParams)
+  .then(function(res){
+    console.log("succes", res.status)
+  })
+}
