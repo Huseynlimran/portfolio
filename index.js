3c4ea518b7177a0e332c49daa45cfde2b4811790
Imran <89724435+Huseynlimran@users.noreply.github.com>
2022-10-29 07:12:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ window.addEventListener("mousemove", (evt) => {
 
 const sg = gsap.timeline({ default: { ease: "power1.out" } });
 
-sg.to(".text", { rotateX: "0deg", duration: 0.2, opacity: 0.7, stagger: 0.1 });
+sg.to(".text", { rotateX: "0deg", duration: 0.2, opacity: 1, stagger: 0.1 });
 
 sg.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 sg.to(".intro", { y: "-100%", duration: 1 }, "-=1");
